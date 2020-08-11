e3f642d5176e0346b2592c3e7a7aeadfe18348e6
Olavo Parno <olavoparno@gmail.com>
2020-08-10 21:42:28 -0300

    fix(project): adjust minor things

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,28 @@
 import * as React from 'react';
 import { useAppendHeadScript } from './useAppendHeadScript';
 
-type TInitHotjar = {
-  hotjarId: string;
-  hotjarVersion: string;
-  shouldLog?: boolean;
-};
-
-type TUserInfo = {
-  userInfo: {
-    id: string;
-  };
-  shouldLog?: boolean;
-};
-
 type TUseHotjar = {
-  initHotjar: ({
-    hotjarId,
-    hotjarVersion,
-    shouldLog,
-  }: TInitHotjar) => Promise<boolean>;
-  identityHotjar: ({ userInfo }: TUserInfo) => Promise<boolean>;
+  initHotjar: (
+    hotjarId: string,
+    hotjarVersion: string,
+    shouldLog?: boolean
+  ) => Promise<boolean>;
+  identityHotjar: (
+    userId: string,
+    userInfo: string,
+    shouldLog?: boolean
+  ) => Promise<boolean>;
 };
 
 export function useHotjar(): TUseHotjar {
   const { appendHeadScript } = useAppendHeadScript();
 
   const initHotjar = React.useCallback(
-    async ({
-      hotjarId,
-      hotjarVersion,
-      shouldLog = true,
-    }: TInitHotjar): Promise<boolean> => {
+    async (
+      hotjarId: string,
+      hotjarVersion: string,
+      shouldLog = true
+    ): Promise<boolean> => {
       const hotjarScript = `(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:${hotjarId},hjsv:${hotjarVersion}};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`;
       const isHotjarAppended = await appendHeadScript({
         scriptText: hotjarScript,
@@ -46,12 +37,13 @@ export function useHotjar(): TUseHotjar {
   );
 
   const identityHotjar = React.useCallback(
-    async ({ userInfo, shouldLog = true }: TUserInfo): Promise<boolean> => {
+    async (
+      userId: string,
+      userInfo: string,
+      shouldLog = true
+    ): Promise<boolean> => {
       try {
-        const { id, ...restUserInfo } = userInfo;
-        const hotjarIdentityScript = `var userId="${id}" || null;window.hj("identify",userId,{${JSON.stringify(
-          restUserInfo
-        )}});`;
+        const hotjarIdentityScript = `var userId="${userId}" || null;window.hj("identify",userId,{${userInfo}}});`;
         const isIdentified = await appendHeadScript({
           scriptText: hotjarIdentityScript,
           scriptId: 'identity-script',
