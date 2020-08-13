1d0259bf7bc42be6c6e3ee47459fd4991eb0743d
Olavo Parno <olavoparno@gmail.com>
2020-08-13 14:08:33 -0300

    chore(project): add terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
@@ -29,5 +30,6 @@ export default {
       clean: true,
     }),
     commonjs(),
+    terser(),
   ],
 };
