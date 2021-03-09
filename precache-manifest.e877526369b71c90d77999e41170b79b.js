c358da86b7878ccddd23a6e7181fa0cfadab7f85
Olavo Parno <olavoparno@gmail.com>
2021-03-09 16:23:09 -0300

    Updates

---
+++ precache-manifest.e877526369b71c90d77999e41170b79b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f133dd5d52d7561693a7966032b3422",
+    "url": "/react-use-hotjar/index.html"
+  },
+  {
+    "revision": "a14f3a9f9fda72eef2e5",
+    "url": "/react-use-hotjar/static/js/2.9d3ff14f.chunk.js"
+  },
+  {
+    "revision": "5942e2044fd50a8ad6c3c5e826f8d0db",
+    "url": "/react-use-hotjar/static/js/2.9d3ff14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "809c9ccb4eadf2cdc644",
+    "url": "/react-use-hotjar/static/js/main.ea1c3906.chunk.js"
+  },
+  {
+    "revision": "ef93dd055d9632f17ca1",
+    "url": "/react-use-hotjar/static/js/runtime-main.b1b3e330.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-use-hotjar/precache-manifest.e877526369b71c90d77999e41170b79b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-use-hotjar/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
